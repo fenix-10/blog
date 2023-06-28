b489b17f71e5ecdfda1a0a5caa927ea02b7409ca
FeNiX <okay85993@gmail.com>
2023-06-28 16:35:32 +0300

    added CRUD for categories in AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,13 @@ Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'],
         Route::get('/', \App\Http\Controllers\Admin\Main\IndexController::class);
     });
     Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
-        Route::get('/', \App\Http\Controllers\Admin\Category\IndexController::class);
+        Route::get('/', \App\Http\Controllers\Admin\Category\IndexController::class)->name('admin.category.index');
+        Route::get('/create', \App\Http\Controllers\Admin\Category\CreateController::class)->name('admin.category.create');
+        Route::post('/', \App\Http\Controllers\Admin\Category\StoreController::class)->name('admin.category.store');
+        Route::get('/{category}', \App\Http\Controllers\Admin\Category\ShowController::class)->name('admin.category.show');
+        Route::get('/{category}/edit', \App\Http\Controllers\Admin\Category\EditController::class)->name('admin.category.edit');
+        Route::patch('/{category}', \App\Http\Controllers\Admin\Category\UpdateController::class)->name('admin.category.update');
+        Route::delete('/{category}', \App\Http\Controllers\Admin\Category\DeleteController::class)->name('admin.category.delete');
     });
 });
 
