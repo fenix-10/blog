a283fd02186554e24f6e0e82a130ef1d4c70a8bf
FeNiX <okay85993@gmail.com>
2023-06-28 17:20:00 +0300

    added CRUD for tags in AdminLTE

--- resources/views/admin/tag/edit.blade.php
+++ resources/views/admin/tag/edit.blade.php
@@ -6,7 +6,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Добавление категории</h1>
+                        <h1 class="m-0">Редактирование тега</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
@@ -25,17 +25,18 @@
                 <!-- Info boxes -->
                 <div class="row">
                     <div class="col-12">
-                        <form action="{{ route('admin.categories.store') }}" method="POST" class="w-25">
+                        <form action="{{ route('admin.tag.update', $tag->id) }}" method="POST" class="w-25">
                             @csrf
+                            @method('PATCH')
                             <div class="form-group">
-                                <input type="text" class="form-control" name="title" placeholder="Название категории">
+                                <input type="text" class="form-control" name="title" placeholder="Название тега" value="{{ $tag->title}}">
                                 @error('title')
                                     <div class="text-danger">
                                         Это поле необходимо для заполнения
                                     </div>
                                 @enderror
                             </div>
-                            <input type="submit" class="btn btn-primary" value="Добавить">
+                            <input type="submit" class="btn btn-primary" value="Обновить">
                         </form>
                     </div>
                 </div>
